0c54054bf99078061c97c196b7327478aa972471
hstrejoluna <trejolunatutoriales@gmail.com>
2024-03-17 17:03:05 -0500

    feat: font sans added

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,28 +28,10 @@ const GlobalStyles = createGlobalStyle`
     font-style: normal
   }
   body, input, select, textarea {
-    font-family: 'ms_sans_serif';
+    font-family: '__ms_sans_serif_c1ce78';
   }
 `
-
-const serif = PT_Serif({
-  variable: '--font-serif',
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  weight: ['400', '700'],
-})
-const sans = Inter({
-  variable: '--font-sans',
-  subsets: ['latin'],
-  // @todo: understand why extrabold (800) isn't being respected when explicitly specified in this weight array
-  // weight: ['500', '700', '800'],
-})
-const mono = IBM_Plex_Mono({
-  variable: '--font-mono',
-  subsets: ['latin'],
-  weight: ['500', '700'],
-})
-
+ 
 export default async function RootLayout({
   children,
 }: {
@@ -58,7 +40,7 @@ export default async function RootLayout({
   return (
     <html
       lang="en"
-      className={`${mono.variable} ${sans.variable} ${serif.variable}`}
+      className={`${ms_sans_serif.className} ${ms_sans_serif_bold.className}`}
     >
       <body>
         <GlobalStyles />
